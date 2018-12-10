41ec24da397d79ced1b586e13b4c939e1d96dddc
lechiaz <chiltth1808034@fpt.edu.vn>
2018-12-10 22:46:54 +0700

    HTML_Assignment

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 
 var btnSubmit = document.forms['register-form']['btnSubmit'];
 btnSubmit.onclick = function () {
-    if (validateForm()) {
-        // Gửi dữ liệu đi.
-        doSubmit();
-    }
+    // if (validateForm()) {
+    //     // Gửi dữ liệu đi.
+    //     doSubmit();
+    // }
+    doSubmit();
 }
 
 
@@ -12,6 +13,11 @@ function doSubmit() {
     var _name = document.forms['register-form']['name'].value;
     var _email = document.forms['register-form']['email'].value;
     var _phone = document.forms['register-form']['phone'].value;
+    var _gender = document.forms['register-form']['gender'].value;
+    var _hobby = document.forms['register-form']['gender'].value;
+    var _info = document.forms['register-form']['info'].value;
+    alert('Họ và tên:' + _name + '\n' + 'Email:' + _email + '\n' + 'Phone:' + _phone + '\n'  +  'Gender:' + _gender + '\n' + 'Hobby:' + _gender + '\n' + 'Info:' + _info);
+    document.forms['register-form'].reset();
 }
 
 function validateForm() {
@@ -51,13 +57,28 @@ function validateForm() {
     if (txtPhone.value == null || txtPhone.value.length == 0) {
         msgPhone.classList.remove('msg-success');
         msgPhone.classList.add('msg-error');
-        msgPhone.innerHTML = 'Email is required!';
+        msgPhone.innerHTML = 'Phone is required!';
         isValidPhone = false;
     } else {
         msgPhone.classList.remove('msg-error');
         msgPhone.classList.add('msg-success');
         msgPhone.innerHTML = 'Ok.';
     }
+    // var isValidGender = true;
+    // var txtGender = document.forms['register-form']['gender'];
+    // var msgGender = txtGender.nextElementSibling;
+    // if (txtGender.checked) {
+    //     alert('1');
+    //     msgGender.classList.remove('msg-success');
+    //     msgGender.classList.add('msg-error');
+    //     msgGender.innerHTML = 'Gender is required!';
+    //     isValidGender = false;
+    // } else {
+    //     alert('2');
+    //     msgGender.classList.remove('msg-error');
+    //     msgGender.classList.add('msg-success');
+    //     msgGender.innerHTML = 'Ok.';
+    // }
 
 
     isValid = isValidName && isValidEmail && isValidPhone;
